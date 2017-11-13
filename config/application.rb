ebf80ce7f42a6f3ae625b65da410a8906aa46dc5
FunnyQ <funnyq@gmail.com>
2017-11-13 17:47:16 +0800

    :sparkles: added config for rails app

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,28 @@ module AppStarter
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Set timezone
+    config.time_zone = 'Taipei'
+    config.active_record.default_timezone = :local
+
+    # Set locale
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = "zh-TW"
+    config.i18n.available_locales = [:"zh-TW", :en]
+    config.i18n.fallbacks = [:en]
+
+    # Set generator
+    config.generators do |g|
+      g.orm :active_record
+      g.view_specs false
+      g.routing_specs false
+      g.helper_specs false
+      g.request_specs false
+      g.assets false
+      g.helper false
+    end
+
+    # ActiveJob
+    config.active_job.queue_adapter = :sidekiq
   end
 end
