fa97ffe6e012446b054416f5014b47afc78578a0
FunnyQ <funnyq@gmail.com>
2017-12-08 19:46:55 +0800

    :books: added doc for Null objects

--- app/models/null/image.rb
+++ app/models/null/image.rb
@@ -1,9 +1,11 @@
-module Null
-  class Image
-    include ActionView::Helpers::AssetUrlHelper
+# Null Object for Image
+#
+# 未指定圖片物件時可使用 Null::Image 替代
+class Null::Image
+  include ActionView::Helpers::AssetUrlHelper
 
-    def url
-      'http://via.placeholder.com/300x300'
-    end
+  # 模擬 Carrierwave 的 `url` 方法給出替代圖片 url
+  def url
+    'http://via.placeholder.com/300x300'
   end
 end
--- app/models/null/user.rb
+++ app/models/null/user.rb
@@ -1,15 +1,19 @@
-module Null
-  class User
-    def name
-      '未登入'
-    end
+# Null Object for User
+#
+# 用來替代 nil 的 User null object
+class Null::User
+  # Name
+  def name
+    '未登入使用者'
+  end
 
-    def email
-      'test@test.com'
-    end
+  # Email
+  def email
+    'test@test.com'
+  end
 
-    def avatar
-      Null::Image.new
-    end
+  # 會產生 Null::Image instance 替代使用者圖像
+  def avatar
+    Null::Image.new
   end
 end
