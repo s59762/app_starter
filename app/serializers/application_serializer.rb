7a1cf026ccf1cfd60446fad42949caf404dd7d77
FunnyQ <funnyq@gmail.com>
2017-12-12 16:26:26 +0800

    :sparkles: added AdminSerializer

---
+++ app/serializers/application_serializer.rb
@@ -0,0 +1,25 @@
+class ApplicationSerializer < ActiveModel::Serializer
+  private
+
+  # 轉換為 unix time
+  #
+  # @param fields [Symble] 要轉換的欄位名稱
+  def self.to_unix_time(*fields)
+    fields.each do |field|
+
+      # 動態產生 methods，並把回傳結果變更為 unix time
+      #
+      # @return [Integer]
+      define_method field do
+        object.send(field).to_i
+      end
+
+      # 動態產生 methods prefix with `raw_`，回傳原始資料
+      #
+      # @return [String]
+      define_method "raw_#{field}" do
+        object.send(field).to_i
+      end
+    end
+  end
+end
