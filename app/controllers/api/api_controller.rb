62a2f956441923b819d41a4b955ea0d2c5c8d7d4
FunnyQ <funnyq@gmail.com>
2017-12-21 16:18:44 +0800

    :sparkles: rescue ApplicationException @ ApiController, and render error message for client

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,11 +1,12 @@
 class Api::ApiController < ActionController::API
   before_action :set_default_format
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ::ApplicationException, with: :show_exception
+
   def set_default_format
     request.format = :json
   end
-
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
 
@@ -15,5 +16,9 @@ class Api::ApiController < ActionController::API
         'resources not found'
       ]
     }, status: :not_found
+  end
+
+  def show_exception(exception)
+    render json: exception.response, status: exception.status
   end
 end
