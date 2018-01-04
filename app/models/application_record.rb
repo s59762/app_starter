041a93bb6acc2033a6945582667980b90501bb91
FunnyQ <funnyq@gmail.com>
2018-01-04 19:21:07 +0800

    :sparkles: added `.latest_updated_at` for all models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,9 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   include BasicScopes
+
+  # 回傳 table 最後更新時間
+  def self.latest_updated_at
+    order(updated_at: :asc).last.updated_at.to_i if exists?
+  end
 end
