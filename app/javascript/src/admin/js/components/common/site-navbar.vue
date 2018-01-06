6d23226f7decbf9251f1cb1148877ec5492f798c
FunnyQ <funnyq@gmail.com>
2018-01-06 22:10:11 +0800

    :sparkles: create site navibar vue component

---
+++ app/javascript/src/admin/js/components/common/site-navbar.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+
+nav.navbar.is-fixed-top.level.animated.slideInDown
+  .nav-left
+    .sidebar-toggle.is-hidden-tablet
+      i.fa.fa-bars
+  .nav-center
+    .brand-logo
+      span
+        b App
+        | Starter
+  .nav-right
+    .options-btn.dropdown.is-right.is-hoverable(:class="optionMenuActive")
+      .dropdown-trigger(@click="triggerOptionMenu")
+        button.button(aria-haspopup='true', aria-controls='dropdown-menu')
+          span.icon.is-small
+            i.fa.fa-gear(aria-hidden='true')
+      .dropdown-menu(role='menu')
+        .dropdown-content
+          a.dropdown-item(href='#')
+            span.icon
+              i.fa.fa-pencil-square-o
+            span Edit Profile
+          hr.dropdown-divider
+          a.dropdown-item(:href="logoutPath"
+                          data-method="delete")
+            span.icon
+              i.fa.fa-sign-out
+            span Log out
+
+</template>
+
+<script>
+export default {
+  // components: {},
+
+  // mixins: {},
+
+  props: {
+    logoutPath: {
+      type: String,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      isOptionMenuOpen: false
+    }
+  },
+
+  computed: {
+    optionMenuActive() {
+      if (this.isOptionMenuOpen == true) { return 'is-active' }
+    }
+  },
+
+  // created() {},
+
+  // mounted() {},
+
+  methods: {
+    triggerOptionMenu() {
+      this.isOptionMenuOpen = !this.isOptionMenuOpen
+    }
+  }
+}
+</script>
---
+++ app/javascript/src/admin/js/vue_initializers/common/site_navbar_container.js
@@ -0,0 +1,23 @@
+import SiteNavbar from '../../components/common/site-navbar.vue'
+
+export default {
+  components: {
+    SiteNavbar
+  }
+
+  // mixins: {},
+
+  // props: {},
+
+  // data() {
+  //   return {}
+  // },
+
+  // computed: {},
+
+  // created() {},
+
+  // mounted() {},
+
+  // methods: {}
+}
