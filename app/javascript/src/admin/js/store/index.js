d8e401766224f6bc4bf924f463ad2e25a0240b3d
FunnyQ <funnyq@gmail.com>
2018-03-01 16:50:58 +0800

    :sparkles: store queryString to vuex at init

--- app/javascript/src/admin/js/store/index.js
+++ app/javascript/src/admin/js/store/index.js
@@ -3,11 +3,13 @@ import Vuex from 'vuex'
 import * as getters from './getters'
 import * as actions from './actions'
 import mutations from './mutations'
+import queryString from 'query-string'
 
 Vue.use(Vuex)
 
 const state = {
   flashMessages: [],
+  queryString: queryString.parse(window.location.search),
   device: {
     isMobile: false
   },
