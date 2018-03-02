38f6424405bbdb9daff6803bfd65d9739e6bf316
FunnyQ <funnyq@gmail.com>
2018-03-02 21:29:10 +0800

    :sparkles: admin list table basic structure with paginate function.

--- app/javascript/src/admin/js/components/admin/list-table.vue
+++ app/javascript/src/admin/js/components/admin/list-table.vue
@@ -1,10 +1,35 @@
 <template lang="pug">
 
-//- do it!
+b-table(:data="admins"
+        paginated
+        backend-pagination
+        :currentPage="currentPage"
+        :perPage="pageSize"
+        @page-change="onPageChange"
+        :total="totalCount"
+        :loading="isLoading"
+        :hoverable="true")
+
+  template(slot-scope="props")
+
+    b-table-column(field="id"
+                   label="ID"
+                   numbric)
+      | {{props.row.id}}
+
+  template(slot='empty')
+    section.section
+      .content.has-text-grey.has-text-centered
+        p
+          b-icon(icon='times'
+                 size='is-large')
+        p 目前沒有資料
 
 </template>
 
 <script>
+import queryString from 'query-string'
+
 export default {
   // components: {},
 
@@ -12,16 +37,91 @@ export default {
 
   // props: {},
 
-  // data() {
-  //   return {}
-  // },
+  data() {
+    return {
+      currentPage: 1,
+      pageSize: 25
+    }
+  },
 
-  // computed: {},
+  computed: {
+    isLoading() {
+      return this.$store.getters['admins/isLoading']
+    },
 
-  // created() {},
+    admins() {
+      return this.$store.getters['admins/allResources']
+    },
+
+    totalCount() {
+      return this.$store.getters['admins/metaInfo'].total_count
+    },
+
+    queryString() {
+      return this.$store.getters['queryString']
+    }
+  },
+
+  /*
+   * 根據這到達此頁時的 URL QueryString 來決定如何送出 request 給 API。
+   *
+   * 在 `options` 變數的賦值過程中檢是否當前的 URL 有給自訂設定，若沒有則使用在 data
+   * 中指定的預設值。接著透過 vuex action 取得 admins list，然後同樣透過 vuex action
+   * 更新 queryString 的內容與 URL。
+   *
+   * 最後監聽 `window.onpopstate` 事件，若發生時（使用者按下瀏覽器的上一頁或下一頁）
+   * 重新取得正確的資料，並更新 vuex store 中 queryString 為當前 URL 的內容。
+   */
+  created() {
+    let options = this.checkCurrentQueryStringOptionsFromURL()
+
+    this.fetchData(options)
+    this.updateQueryString(options)
+
+    window.onpopstate = event => {
+      this.fetchData(event.state)
+      this.$store.dispatch('updateQueryStringFromURL')
+    }
+  },
 
   // mounted() {},
 
-  // methods: {}
+  methods: {
+    checkCurrentQueryStringOptionsFromURL() {
+      let currentQueryString = queryString.parse(window.location.search)
+      let options = {
+        pageNumber: parseInt(currentQueryString['page[number]']) || this.currentPage,
+        pageSize: parseInt(currentQueryString['page[size]']) || this.pageSize
+      }
+
+      this.currentPage = options.pageNumber
+      this.pageSize = options.pageSize
+
+      return options
+    },
+
+    onPageChange(page) {
+      let options = {
+        pageNumber: page,
+        pageSize: this.pageSize
+      }
+      this.fetchData(options)
+      this.updateQueryString(options)
+    },
+
+    fetchData(options) {
+      this.currentPage = options.pageNumber
+      this.$store.dispatch('admins/fetchAllResources', options)
+    },
+
+    updateQueryString(options) {
+      this.$store.dispatch('updateQueryString', {
+        options,
+        newQueryString: `/admin/admins/?page[number]=${options.pageNumber}&page[size]=${
+          options.pageSize
+        }`
+      })
+    }
+  }
 }
 </script>
--- app/javascript/src/admin/js/store/actions.js
+++ app/javascript/src/admin/js/store/actions.js
@@ -23,13 +23,22 @@ export const toggleSidebar = ({ commit }, option) => {
   commit(types.TOGGLE_SIDEBAR, option)
 }
 
-export const updateQueryString = ({ commit }, { stateObj, newQueryString }) => {
+/**
+ * 用 pushState 更新 URL，同時更新 queryString 的內容
+ *
+ * @param {object} options 提供 `window.onpopstate` 使用，傳入的物件內容會包含在 popState event 的 state
+ * @param {string} newQueryString 新的 URL path，盡量使用完整的絕對路徑比較不容易出錯。
+ */
+export const updateQueryString = ({ commit }, { options, newQueryString }) => {
   let title = document.title
 
-  window.history.pushState(stateObj, title, newQueryString)
+  window.history.pushState(options, title, newQueryString)
   commit(types.UPDATE_QUERY_STRING)
 }
 
+/**
+ * 重新把當前的 URL Query string 放到 vuex store 中。通常是給 window.onpopstate 使用的。
+ */
 export const updateQueryStringFromURL = ({ commit }) => {
   commit(types.UPDATE_QUERY_STRING)
 }
