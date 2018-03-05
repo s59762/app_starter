bdcc7a5d181c806655d091efa440f65f14887421
FunnyQ <funnyq@gmail.com>
2018-03-05 17:30:03 +0800

    :sparkles: added vuejs plugin for i18n translation

---
+++ app/javascript/src/shared/plugins/vue_locale_translater.js
@@ -0,0 +1,94 @@
+import i18n from 'i18n-js'
+
+function install(Vue) {
+  Vue.mixin({
+    methods: {
+
+      /**
+       * 取得 Rails model attributes 的 i18n 值
+       *
+       * @param {string} model model name
+       * @param {string} attribute attribute name
+       * @returns {string}
+       */
+      attributeLocaleText(model, attribute) {
+        return i18n.t(`activerecord.attributes.${model}.${attribute}`)
+      },
+
+      /**
+       * 取得 Rails model name 的 i18n 值
+       *
+       * @param {string} model model name
+       * @param {boolean} isPlural 是否為複數（預設為 false）
+       * @returns {string}
+       */
+      modelNameLocaleText(model, isPlural = false) {
+        if (isPlural) {
+          return i18n.t(`activerecord.models.${model}.others`)
+        } else {
+          return i18n.t(`activerecord.models.${model}.one`)
+        }
+      },
+
+      /**
+       * 取得 Rails model 中 enums 的 i18n 值
+       *
+       * @param {string} model model name
+       * @param {string} enumAttribute enum 欄位的名稱
+       * @param {string} key enum 值的名稱
+       * @returns {string}
+       */
+      enumsLocaleText(model, enumAttribute, key) {
+        return i18n.t(`enums.${model}.${enumAttribute}.${key}`)
+      },
+
+      /**
+       * 取得 Rails model 中 aasm state 的 i18n 值
+       *
+       * @param {string} model model name
+       * @param {string} aasmAttribute 狀態的名稱
+       * @param {string} state 狀態值的名稱
+       * @returns {string}
+       */
+      aasmStateLocaleText(model, aasmAttribute, state) {
+        return i18n.t(`aasm_state.${model}.${aasmAttribute}.${state}`)
+      },
+
+      /**
+       * 取得 Rails model 中 public activity 的 i18n 值
+       *
+       * @param {string} model model name
+       * @param {string} activity 活動的名稱
+       * @param {object} options 若需要可帶入 locals 物件
+       * @returns {string}
+       */
+      activityLocaleText(model, activity, options = {}) {
+        return i18n.t(`activity.${model}.${activity}`, options)
+      },
+
+      /**
+       * 取得頁面標題的 i18n 值
+       *
+       * @param {string} pageName 頁面名稱
+       * @param {string} subPageName 副頁面名稱
+       * @returns {string}
+       */
+      pageTitleLocaleText(pageName, subPageName) {
+        return i18n.t(`page_titles.${pageName}.${subPageName}`)
+      },
+
+      /**
+       * 取得 action 的 i18n 值
+       *
+       * @param {string} scope 使用者角色
+       * @param {string} action 動作的名稱
+       * @returns {string}
+       */
+      actionLocaleText(scope, action) {
+        return i18n.t(`actions.${scope}.${action}`)
+      }
+    }
+  })
+}
+
+export default install
