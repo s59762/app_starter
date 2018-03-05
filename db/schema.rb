cf0776c8e5fdb5cb7bbfb9ac4b34d6f1143b962a
FunnyQ <funnyq@gmail.com>
2018-03-05 20:59:55 +0800

    :sparkles: add is_suspended to admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171211061403) do
+ActiveRecord::Schema.define(version: 20180305125703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171211061403) do
     t.string "avatar"
     t.string "name"
     t.integer "role", default: 0
+    t.boolean "is_suspended", default: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
