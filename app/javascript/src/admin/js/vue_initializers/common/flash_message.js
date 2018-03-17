5c971d523487ab667036990bae62bcd8277a9af1
FunnyQ <funnyq@gmail.com>
2018-03-17 12:05:49 +0800

    :sparkles: added debounce to window.resize event

--- app/javascript/src/admin/js/vue_initializers/common/flash_message.js
+++ app/javascript/src/admin/js/vue_initializers/common/flash_message.js
@@ -1,5 +1,6 @@
 import FlashMessage from '../../components/common/flash-message.vue'
 import { mapGetters } from 'vuex'
+import debounce from 'lodash.debounce'
 
 export default {
   components: {
@@ -10,8 +11,37 @@ export default {
     ...mapGetters(['flashMessages'])
   },
 
+  beforeMount() {
+    const { body } = document
+    const DISKTOP_WIDTH = 1024
+    const RATIO = 3
+    const handler = () => {
+      if (!document.hidden) {
+        let rect = body.getBoundingClientRect()
+        let isMobile = rect.width - RATIO < DISKTOP_WIDTH
+
+        this.toggleDevice(isMobile ? 'mobile' : 'other')
+        this.toggleSidebar(!isMobile)
+      }
+    }
+
+    document.addEventListener('visibilitychange', handler)
+    window.addEventListener('resize', debounce(handler, 200))
+    handler()
+  },
+
   mounted() {
     let messages = JSON.parse(this.$el.dataset.messages)
     this.$store.dispatch('setFlashMessages', messages)
+  },
+
+  methods: {
+    toggleDevice(device) {
+      this.$store.dispatch('toggleDevice', device)
+    },
+
+    toggleSidebar(option) {
+      this.$store.dispatch('toggleSidebar', option)
+    }
   }
 }
