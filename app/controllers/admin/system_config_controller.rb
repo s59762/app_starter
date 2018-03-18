aedb4bf7bed330c984342e6863141ef011aa5145
FunnyQ <funnyq@gmail.com>
2018-03-18 18:49:36 +0800

    :sparkles: added system comfig page basic structure

--- app/controllers/admin/system_config_controller.rb
+++ app/controllers/admin/system_config_controller.rb
@@ -2,6 +2,6 @@ require_dependency 'admin/application_controller'
 
 class Admin::SystemConfigController < Admin::ApplicationController
   def show
-    #
+    @meta_tags_config = SiteConfig.meta_tags
   end
 end
