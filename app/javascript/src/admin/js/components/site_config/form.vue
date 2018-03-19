613112732fdf6320f42ed73156b4a967b88c0b7d
FunnyQ <funnyq@gmail.com>
2018-03-19 20:54:03 +0800

    :art: refine RWD

--- app/javascript/src/admin/js/components/site_config/form.vue
+++ app/javascript/src/admin/js/components/site_config/form.vue
@@ -4,62 +4,60 @@
   .box-header.with-border
     h2.box-title {{pageTitleLocaleText('admin', 'system_config', 'meta_tags_config')}}
   .box-body
-    .box.form-container-box.is-default
-      section.section.meta-tags-config
-        h3.section-title {{pageTitleLocaleText('admin', 'system_config', 'site_title_style')}}
-        .columns
-          .column.is-7
-            b-field(:label="attributeLocaleText('site_config', 'site_name')"
-                    :type="form.errorClassAt('site_name')"
-                    :message="form.errors.get('site_name')"
-                    horizontal)
-              b-input(type="text"
-                      placeholder="Your Site Name"
-                      v-model="form.site_name"
-                      @input="form.errors.clear('site_name')")
+    section.section.meta-tags-config
+      h3.section-title {{pageTitleLocaleText('admin', 'system_config', 'site_title_style')}}
+      .columns
+        .column.is-7
+          b-field(:label="attributeLocaleText('site_config', 'site_name')"
+                  :type="form.errorClassAt('site_name')"
+                  :message="form.errors.get('site_name')")
+            b-input(type="text"
+                    placeholder="Your Site Name"
+                    v-model="form.site_name"
+                    @input="form.errors.clear('site_name')")
 
-            b-field(:label="attributeLocaleText('site_config', 'separator')"
-                    :type="form.errorClassAt('separator')"
-                    :message="form.errors.get('separator')"
-                    horizontal)
-              b-input(type="text"
-                      placeholder="e.g. :: or ||"
-                      v-model="form.separator"
-                      @input="form.errors.clear('separator')")
-            b-field(:label="attributeLocaleText('site_config', 'reverse')"
-                    :type="form.errorClassAt('reverse')"
-                    :message="form.errors.get('reverse')"
-                    horizontal)
-              b-switch(v-model="form.reverse"
-                       size="is-large"
-                       type="is-success"
-                       @input="form.errors.clear('reverse')")
-          .style-preview-container.column.is-5
-            h4.subtitle {{pageTitleLocaleText('admin', 'system_config', 'style_preview')}}
-            .browser-mock
-              span.tab {{titlePreview}}
-              .url-input http://www.my-website.com
+          b-field(:label="attributeLocaleText('site_config', 'separator')"
+                  :type="form.errorClassAt('separator')"
+                  :message="form.errors.get('separator')")
+            b-input(type="text"
+                    placeholder="e.g. :: or ||"
+                    v-model="form.separator"
+                    @input="form.errors.clear('separator')")
+          b-field(:label="attributeLocaleText('site_config', 'reverse')"
+                  :type="form.errorClassAt('reverse')"
+                  :message="form.errors.get('reverse')")
+            b-switch(v-model="form.reverse"
+                      size="is-large"
+                      type="is-success"
+                      @input="form.errors.clear('reverse')")
+        .style-preview-container.column.is-5
+          h4.subtitle {{pageTitleLocaleText('admin', 'system_config', 'style_preview')}}
+          .browser-mock
+            span.tab {{titlePreview}}
+            .url-input http://www.my-website.com
 
-      section.section.seo-config
-        h3.section-title {{pageTitleLocaleText('admin', 'system_config', 'seo_config')}}
-        .columns
-          .column.is-7
-            b-field(:label="attributeLocaleText('site_config', 'description')"
-                    :type="form.errorClassAt('description')"
-                    :message="form.errors.get('description')"
-                    horizontal)
-              b-input(type="text"
-                      placeholder="e.g. Jone Doe"
-                      v-model="form.description"
-                      @input="form.errors.clear('description')")
-            b-field(:label="attributeLocaleText('site_config', 'keywords')"
-                    :type="form.errorClassAt('keywords')"
-                    :message="form.errors.get('keywords')"
-                    horizontal)
-              b-input(type="text"
-                      placeholder="e.g. Jone Doe"
-                      v-model="form.keywords"
-                      @input="form.errors.clear('keywords')")
+    section.section.seo-config
+      h3.section-title {{pageTitleLocaleText('admin', 'system_config', 'seo_config')}}
+      .columns
+        .column.is-7
+          b-field(:label="attributeLocaleText('site_config', 'description')"
+                  :type="form.errorClassAt('description')"
+                  :message="form.errors.get('description')")
+            b-input(type="textarea"
+                    placeholder="e.g. Jone Doe"
+                    v-model="form.description"
+                    @input="form.errors.clear('description')"
+                    has-counter)
+          p.help 建議不超過 140 字
+          br
+          b-field(:label="attributeLocaleText('site_config', 'keywords')"
+                  :type="form.errorClassAt('keywords')"
+                  :message="form.errors.get('keywords')")
+            b-input(type="text"
+                    placeholder="e.g. Jone Doe"
+                    v-model="form.keywords"
+                    @input="form.errors.clear('keywords')")
+          p.help 請以半形逗點分開，建議不超過 3 個
 
     .is-pulled-right
       .button.is-primary(@click="updateMetaTags") {{actionLocaleText('admin', 'update_config')}}
