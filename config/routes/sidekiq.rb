587748bba2c32e23a26e7d45b29e64a167c2ba11
FunnyQ <funnyq@gmail.com>
2018-03-22 18:01:10 +0800

    :books:

--- config/routes/sidekiq.rb
+++ config/routes/sidekiq.rb
@@ -1,6 +1,6 @@
 # sidekiq monitor web UI
 if Rails.env.development?
-  mount Sidekiq::Web => "/admin/sidekiq"
+  mount Sidekiq::Web => '/admin/sidekiq'
 else
   authenticate :admin do
     mount Sidekiq::Web => '/admin/sidekiq'
