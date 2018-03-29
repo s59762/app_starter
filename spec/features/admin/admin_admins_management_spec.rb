3492972826e0f7bbf47a33116003217b51105302
FunnyQ <funnyq@gmail.com>
2018-03-29 13:55:48 +0800

    :books: change syntaxt

--- spec/features/admin/admin_admins_management_spec.rb
+++ spec/features/admin/admin_admins_management_spec.rb
@@ -7,40 +7,53 @@ RSpec.feature "Admin::AdminsManagement", type: :feature do
   before(:each) do
     login_as_default_admin
     visit admin_admins_path
+
+    find('[data-behavior="admin-list"] tbody tr', match: :first)
   end
 
   describe '成員列表' do
-    it '應該要顯示所有成員' do
+    scenario '應該要顯示所有成員', js: true do
       # 包含 default admin 應該要有三個成員
       expect(page).to have_selector('[data-behavior="admin-list"] tbody tr', count: 3)
     end
   end
 
-  describe '新增成員', js: true do
-    it '可以透過新增成員按鈕打開表單，新增一個成員' do
-      find('[data-behavior="new-admin-button"]').click
+  describe '新增成員' do
+    scenario '可以透過新增成員按鈕打開表單，新增一個成員', js: true do
+      open_new_admin_form
 
       # search user 1
       find('[data-behavior="admin-email"]').set 'test@test.com'
       find('[data-behavior="admin-name"]').set '測試成員'
       find('option[value="accounter"]').click
 
-      find('[data-behavior="submit-button"]').click
+      click_submit_button
 
       expect(page).to have_content('測試成員')
     end
   end
 
   describe '編輯成員', js: true do
-    it '可以透過編輯按鈕修改成員資料' do
-      find('[data-behavior="admin-list"] tbody tr', match: :first)
-      all('[data-behavior="admin-list"] tbody tr')[1].find('[data-behavior="edit-button"]').click
+    scenario '可以透過編輯按鈕修改成員資料', js: true do
+      open_edit_admin_form
 
       find('[data-behavior="admin-name"]').set '編輯過了'
 
-      find('[data-behavior="submit-button"]').click
+      click_submit_button
 
       expect(page).to have_content('編輯過了')
     end
+  end
+
+  def open_new_admin_form
+    find('[data-behavior="new-admin-button"]').click
+  end
+
+  def open_edit_admin_form
+    all('[data-behavior="admin-list"] tbody tr')[1].find('[data-behavior="edit-button"]').click
+  end
+
+  def click_submit_button
+    find('[data-behavior="submit-button"]').click
   end
 end
--- spec/features/admin/admin_users_management_spec.rb
+++ spec/features/admin/admin_users_management_spec.rb
@@ -9,30 +9,38 @@ RSpec.feature "Admin::UsersManagement", type: :feature do
     visit admin_users_path
   end
 
-  describe '使用者管理介面列表', js: true do
-    it '可以列出所有使用者' do
+  describe '使用者管理介面列表' do
+    scenario '可以列出所有使用者', js: true do
       expect(page).to have_content(user_1.email)
       expect(page).to have_content(user_2.email)
     end
 
-    it '可以透過完整 Email 搜尋找到特定使用者' do
-      find('[data-behavior="search-user-panel"]').click
+    scenario '可以透過完整 Email 搜尋找到特定使用者', js: true do
+      open_search_panel
 
       find('[data-behavior="search-by-user-email-field"]').set user_2.email
-      find('[data-behavior="search-button"]').click
+      click_search_button
 
       expect(page).to have_content(user_2.email)
       expect(page).to have_no_content(user_1.email)
     end
 
-    it '可以透過 Email 模糊搜尋找到特定使用者' do
-      find('[data-behavior="search-user-panel"]').click
+    scenario '可以透過 Email 模糊搜尋找到特定使用者', js: true do
+      open_search_panel
 
       find('[data-behavior="search-by-user-email-field"]').set user_1.email.split('@').first
-      find('[data-behavior="search-button"]').click
+      click_search_button
 
       expect(page).to have_content(user_1.email)
       expect(page).to have_no_content(user_2.email)
     end
+  end
+
+  def open_search_panel
+    find('[data-behavior="search-user-panel"]').click
+  end
+
+  def click_search_button
+    find('[data-behavior="search-button"]').click
   end
 end
