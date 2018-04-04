aae1e4769078336e00b0d39363114d90626e8376
FunnyQ <funnyq@gmail.com>
2018-04-04 19:52:12 +0800

    :sparkles: added product list-table component

---
+++ app/javascript/src/admin/js/components/product/list-table.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+
+.product-list-table
+
+</template>
+
+<script>
+export default {
+  // components: {},
+
+  // mixins: [],
+
+  // props: {},
+
+  // data() {
+  //   return {}
+  // },
+
+  // computed: {},
+
+  // created() {},
+
+  // mounted() {},
+
+  // methods: {}
+}
+</script>
