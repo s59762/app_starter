c832f9c87d4e10f487ad3a1d9e78209a9bdfd7e7
FunnyQ <funnyq@gmail.com>
2018-04-06 22:15:07 +0800

    :sparkles: added create product page basic structure.

---
+++ app/javascript/src/admin/js/vue_initializers/product/new_product_page_container.js
@@ -0,0 +1,23 @@
+import NewProductForm from '../../components/product/new-form'
+
+export default {
+  components: {
+    NewProductForm
+  }
+
+  // mixins: [],
+
+  // props: {},
+
+  // data() {
+  //   return {}
+  // },
+
+  // computed: {},
+
+  // created() {},
+
+  // mounted() {},
+
+  // methods: {}
+}
