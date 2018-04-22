f49415d3d06fe67cb737ba3539f4d79ec440578e
FunnyQ <funnyq@gmail.com>
2018-04-22 16:16:31 +0800

    :sparkles: added user scope option for data cache service

--- app/services/api/data_cache_service.rb
+++ app/services/api/data_cache_service.rb
@@ -1,6 +1,6 @@
 # 嘗試先看看 request 的內容有沒有 cached 的資料，有的話使用 cache，沒有的話才 render json
 class Api::DataCacheService
-  attr_reader :collection, :request, :meta_options, :extra_options
+  attr_reader :collection, :request, :meta_options, :extra_options, :user_scope
 
   def initialize(collection, request, **options)
     @collection = collection
@@ -8,10 +8,11 @@ class Api::DataCacheService
 
     @meta_options = options.fetch(:meta, {})
     @extra_options = options.fetch(:extra, {})
+    @user_scope = options.fetch(:user_scope, nil)
   end
 
   def call
-    Rails.cache.fetch(collection.cache_key) do
+    Rails.cache.fetch(cache_key_with_user_scope) do
       show_indicator_in_development_log
       ActiveModelSerializers::SerializableResource.new(collection, structured_options).as_json
     end
@@ -24,18 +25,37 @@ class Api::DataCacheService
   # @param [Object] collection ActiveRecord::Relation 物件或是單筆 model 的 instance。通常是使用 FetchingDataService 回傳的內容
   # @see FetchingDataService.call
   # @param [Object] request 從 controller 中得到的 request 物件
-  # @param [Hash] options 可傳入要給 AMS 的額外 options。 options[:meta] 會加入 AMS 的 meta 中，而 options[:extra] 則是 AMS 的 include 內容與 instance_options
+  # @param [Hash] options 可傳入要給 AMS 的額外 options。 options[:meta] 會加入 AMS 的 meta 中，而 options[:extra] 則是 AMS 的 include 內容與 instance_options。options[:user_scope] 可指定目前使用 API 的使用者，一般可使用 Model class name。
   #
   #
   # @example 要一併 render 出 Article 的 Comments，並在 meta 中加入 comment_count
   #   # API::V1::ArticlesController#index
   #   # ...
   #     result = Api::DataCacheService.call(@articles,
-  #                                               request,
-  #                                               meta: { article_count: Article.count },
-  #                                               extra: { include: [:comments],
-  #                                                        show_comments: true })
+  #                                         request,
+  #                                         meta: { article_count: Article.count },
+  #                                         extra: { include: [:comments],
+  #                                                  show_comments: true })
   #     render json: result
+  #   # ...
+  #
+  # @example 要輸出 PostInfo，若有些個人隱私資料會依照 user 的不同而必須有差異時可透過 user scope 與不同的 serializer 來把資料區分開來
+  #   # API::V1::PostsController#index
+  #   # ...
+  #     result = Api::DataCacheService.call(@articles,
+  #                                         request,
+  #                                         extra: extra_options,
+  #                                         user_scope: current_api_user.class.name)
+  #     render json: result
+  #   # ...
+  #   def extra_options
+  #     return {} unless admin_signed_in?
+
+  #     {
+  #       each_serializer: EventPostSerializer::Detail,
+  #       show_prize: true
+  #     }
+  #   end
   #   # ...
   #
   # @return [Hash] 會回傳 Cache 或直接透過 `ActiveModelSerializers::SerializableResource#as_json` 得到的內容
@@ -66,6 +86,11 @@ class Api::DataCacheService
   end
 
   private
+
+  def cache_key_with_user_scope
+    return collection.cache_key unless user_scope
+    "#{user_scope}-#{collection.cache_key}"
+  end
 
   def structured_options
     @options ||= {
