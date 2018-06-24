211e86be2ccbbb90a4869f564e170dd539d6b362
FunnyQ <funnyq@gmail.com>
2018-06-24 19:51:58 +0800

    :sparkles: add brand new button

--- app/javascript/src/admin/js/components/brand/new-button.vue
+++ app/javascript/src/admin/js/components/brand/new-button.vue
@@ -1,20 +1,37 @@
 <template lang="pug">
 
 .vc-brand-new-button
+  button.button.is-small.is-primary(@click="showForm"
+                                    data-behavior="new-brand-button")
+    span.icon
+      i.fa.fa-plus
+    span {{ actionLocaleText('admin', 'create', { model: modelNameLocaleText('brand') }) }}
+  b-modal(:active.sync="isFormActive")
+    brand-form
 
 </template>
 
 <script>
+import BrandForm from './form.vue'
+
 export default {
-  // components: {},
+  components: {
+    BrandForm
+  },
   // mixins: [],
   // props: {},
-  // data() {
-  //   return {}
-  // },
+  data() {
+    return {
+      isFormActive: false
+    }
+  },
   // computed: {},
   // created() {},
   // mounted() {},
-  // methods: {}
+  methods: {
+    showForm() {
+      this.isFormActive = true
+    }
+  }
 }
 </script>
