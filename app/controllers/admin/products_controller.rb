563845e4027fd9e946abede04355e1a8c5497e65
FunnyQ <funnyq@gmail.com>
2018-06-25 21:06:59 +0800

    :sparkles: add flash message after add or update product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,6 +3,7 @@ require_dependency 'admin/application_controller'
 class Admin::ProductsController < Admin::ApplicationController
   def index
     authorize :product, :index?
+    show_flash_message
   end
 
   def new
@@ -13,5 +14,17 @@ class Admin::ProductsController < Admin::ApplicationController
     @product = Product.find(params[:id])
 
     authorize @product, :update?
+  end
+
+  private
+
+  def show_flash_message
+    product_added = params[:product_added] && request.referer.split('/')[-2] == 'products' && request.referer.split('/')[-1] == 'new'
+    product_updated = params[:product_updated] && request.referer.split('/')[-3] == 'products' && request.referer.split('/')[-1] == 'edit'
+
+    return unless product_added || product_updated
+
+    flash.now[:success] = I18n.t('messages.resource_added_successfully', resource: Product.model_name.human) if product_added
+    flash.now[:success] = I18n.t('messages.resource_updated_successfully', resource: Product.model_name.human) if product_updated
   end
 end
