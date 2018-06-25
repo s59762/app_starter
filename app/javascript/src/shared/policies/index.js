8b9a92aff456e84765cf56db39a18d239276efde
FunnyQ <funnyq@gmail.com>
2018-06-25 19:09:47 +0800

    :sparkles: add product category policies

--- app/javascript/src/shared/policies/index.js
+++ app/javascript/src/shared/policies/index.js
@@ -1,9 +1,11 @@
+import ProductCategoryPolicy from './product_category_policy'
 import BrandPolicy from './brand_policy'
 import UserPolicy from './user_policy'
 import AdminPolicy from './admin_policy'
 import DashboardPolicy from './dashboard_policy'
 
 const POLICIES = {
+  productCategory: ProductCategoryPolicy,
   brand: BrandPolicy,
   user: UserPolicy,
   admin: AdminPolicy,
