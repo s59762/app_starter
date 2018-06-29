9452700ef56ffd7266a24badde579c35dbfe0ca9
FunnyQ <funnyq@gmail.com>
2018-06-29 19:49:05 +0800

    :package: move properties columns to seprated component

--- app/javascript/src/admin/js/components/product/form.vue
+++ app/javascript/src/admin/js/components/product/form.vue
@@ -90,7 +90,7 @@
         //- option types
         option-types-columns(v-if="product.isNewRecord()"
                              :errors="errors"
-                             :option-types="form.option_types")
+                             :option-types.sync="form.option_types")
 
         //- options
         //- section.section.product-options-wrapper
@@ -104,76 +104,8 @@
         //-               @input="errors.clear('is_preorder')")
         //-       | {{enumLocaleText('product', 'is_preorder', form.is_preorder)}}
 
-        properties
-        section.section
-          h4.section-title {{pageTitleLocaleText('admin', 'products', 'property_fields')}}
-
-          .properties-wrapper
-            //- 基本商品屬性（體積、重量）
-            h5.sub-title {{pageTitleLocaleText('admin', 'products', 'basic_properties')}}
-            .property-fields(v-for="basicProperty in basicProperties")
-              .columns
-                .column
-                  b-field(:label="attributeLocaleText('product', 'property_name')"
-                          :type="errors.errorClassAt('properties')")
-                    b-input(type="text"
-                            :value="attributeLocaleText('product', basicProperty.name)"
-                            disabled)
-                .column.is-6
-                  b-field(:label="attributeLocaleText('product', 'property_value')"
-                          :type="errors.errorClassAt('properties')"
-                          :message="errors.get(basicProperty.name)")
-                    b-input(type="number"
-                            placeholder="e.g. 80"
-                            v-model="form[basicProperty.name]"
-                            data-behavior="product-width-value"
-                            @input="errors.clear(basicProperty.name)")
-                .column
-                  b-field(:label="attributeLocaleText('product', 'property_unit')"
-                          :type="errors.errorClassAt('properties')")
-                    b-input(type="text"
-                            :value="basicProperty.unit"
-                            disabled)
-            //- 自訂商品屬性
-            h5.sub-title(v-if="form.properties.length > 0") {{pageTitleLocaleText('admin', 'products', 'extra_properties')}}
-            .property-fields(v-for="(property, index) in form.properties")
-              .property-count {{index + 1}}
-              .delete-button(@click="deleteProperty(index)")
-                i.fa.fa-close
-              .columns
-                .column
-                  b-field(:label="attributeLocaleText('product', 'property_name')"
-                          :type="errors.errorClassAt('properties')"
-                          :message="errors.get('properties')")
-                    b-input(type="text"
-                            placeholder="e.g. Battery"
-                            v-model="property.name"
-                            data-behavior="product-property-name"
-                            @input="errors.clear('properties')")
-                .column.is-6
-                  b-field(:label="attributeLocaleText('product', 'property_value')"
-                          :type="errors.errorClassAt('properties')"
-                          :message="errors.get('properties')")
-                    b-input(type="text"
-                            placeholder="e.g. 12000"
-                            v-model="property.value"
-                            data-behavior="product-property-value"
-                            @input="errors.clear('properties')")
-                .column
-                  b-field(:label="attributeLocaleText('product', 'property_unit')"
-                          :type="errors.errorClassAt('properties')"
-                          :message="errors.get('properties')")
-                    b-input(type="text"
-                            placeholder="e.g. mAh"
-                            v-model="property.unit"
-                            data-behavior="product-property-unit"
-                            @input="errors.clear('properties')")
-
-          .add-property-button.button.is-default.is-block(@click="addProperty"
-                                                          data-behavior="product-add-property-button")
-            .icon
-              i.fa.fa-plus
-            span {{actionLocaleText('admin', 'add_product_property')}}
+        properties-columns(:errors="errors"
+                           :form.sync="form")
 
         br
 
@@ -195,6 +127,7 @@ import Product from '../../../../shared/resource_models/product'
 import Form from 'odd-form_object'
 import PriceInfoColumns from './price-info-columns.vue'
 import OptionTypesColumns from './option-types-columns.vue'
+import PropertiesColumns from './propertiess-columns.vue'
 
 const toolbarOptions = [
   [{ size: [false, 'small', 'large', 'huge'] }], // custom dropdown
@@ -213,13 +146,6 @@ const action = 'products/uploadImages'
 const additionalFormData = formData => {
   formData.append('product[use_case]', 'description')
 }
-const propertyTemplate = () => {
-  return {
-    name: '',
-    value: '',
-    unit: ''
-  }
-}
 
 Quill.register('modules/ImageHandler', ImageHandler)
 // Quill.register('modules/ImageResize', ImageResize)
@@ -228,7 +154,8 @@ export default {
   components: {
     quillEditor,
     PriceInfoColumns,
-    OptionTypesColumns
+    OptionTypesColumns,
+    PropertiesColumns
   },
 
   // mixins: [],
@@ -304,27 +231,6 @@ export default {
       return this.$store.getters['brands/all']
     },
 
-    basicProperties() {
-      return [
-        {
-          name: 'width',
-          unit: 'CM'
-        },
-        {
-          name: 'depth',
-          unit: 'CM'
-        },
-        {
-          name: 'height',
-          unit: 'CM'
-        },
-        {
-          name: 'weight',
-          unit: 'KG'
-        }
-      ]
-    },
-
     topLevelCategories() {
       return this.categories.filter(category => category.parent_id === null)
     },
@@ -365,14 +271,6 @@ export default {
   methods: {
     findCategoryBy(id) {
       return this.categories.find(category => category.id == id)
-    },
-
-    addProperty() {
-      this.form.properties.push(propertyTemplate())
-    },
-
-    deleteProperty(index) {
-      this.form.properties.splice(index, 1)
     },
 
     /*
---
+++ app/javascript/src/admin/js/components/product/propertiess-columns.vue
@@ -0,0 +1,135 @@
+<template lang="pug">
+
+section.vc-product-propertiess-columns.section
+  h4.section-title {{pageTitleLocaleText('admin', 'products', 'property_fields')}}
+
+  .properties-wrapper
+    //- 基本商品屬性（體積、重量）
+    h5.sub-title {{pageTitleLocaleText('admin', 'products', 'basic_properties')}}
+    .property-fields(v-for="basicProperty in basicProperties")
+      .columns
+        .column
+          b-field(:label="attributeLocaleText('product', 'property_name')"
+                  :type="errors.errorClassAt('properties')")
+            b-input(type="text"
+                    :value="attributeLocaleText('product', basicProperty.name)"
+                    disabled)
+        .column.is-6
+          b-field(:label="attributeLocaleText('product', 'property_value')"
+                  :type="errors.errorClassAt('properties')"
+                  :message="errors.get(basicProperty.name)")
+            b-input(type="number"
+                    placeholder="e.g. 80"
+                    v-model="form[basicProperty.name]"
+                    data-behavior="product-width-value"
+                    @input="errors.clear(basicProperty.name)")
+        .column
+          b-field(:label="attributeLocaleText('product', 'property_unit')"
+                  :type="errors.errorClassAt('properties')")
+            b-input(type="text"
+                    :value="basicProperty.unit"
+                    disabled)
+    //- 自訂商品屬性
+    h5.sub-title(v-if="form.properties.length > 0") {{pageTitleLocaleText('admin', 'products', 'extra_properties')}}
+    .property-fields(v-for="(property, index) in form.properties")
+      .property-count {{index + 1}}
+      .delete-button(@click="deleteProperty(index)")
+        i.fa.fa-close
+      .columns
+        .column
+          b-field(:label="attributeLocaleText('product', 'property_name')"
+                  :type="errors.errorClassAt('properties')"
+                  :message="errors.get('properties')")
+            b-input(type="text"
+                    placeholder="e.g. Battery"
+                    v-model="property.name"
+                    data-behavior="product-property-name"
+                    @input="errors.clear('properties')")
+        .column.is-6
+          b-field(:label="attributeLocaleText('product', 'property_value')"
+                  :type="errors.errorClassAt('properties')"
+                  :message="errors.get('properties')")
+            b-input(type="text"
+                    placeholder="e.g. 12000"
+                    v-model="property.value"
+                    data-behavior="product-property-value"
+                    @input="errors.clear('properties')")
+        .column
+          b-field(:label="attributeLocaleText('product', 'property_unit')"
+                  :type="errors.errorClassAt('properties')"
+                  :message="errors.get('properties')")
+            b-input(type="text"
+                    placeholder="e.g. mAh"
+                    v-model="property.unit"
+                    data-behavior="product-property-unit"
+                    @input="errors.clear('properties')")
+
+  .add-property-button.button.is-default.is-block(@click="addProperty"
+                                                  data-behavior="product-add-property-button")
+    .icon
+      i.fa.fa-plus
+    span {{actionLocaleText('admin', 'add_product_property')}}
+
+</template>
+
+<script>
+const propertyTemplate = () => {
+  return {
+    name: '',
+    value: '',
+    unit: ''
+  }
+}
+
+export default {
+  // components: {},
+  // mixins: [],
+  props: {
+    errors: {
+      type: Object,
+      required: true
+    },
+
+    form: {
+      type: Object,
+      required: true
+    }
+  },
+  // data() {
+  //   return {}
+  // },
+  computed: {
+    basicProperties() {
+      return [
+        {
+          name: 'width',
+          unit: 'CM'
+        },
+        {
+          name: 'depth',
+          unit: 'CM'
+        },
+        {
+          name: 'height',
+          unit: 'CM'
+        },
+        {
+          name: 'weight',
+          unit: 'KG'
+        }
+      ]
+    }
+  },
+  // created() {},
+  // mounted() {},
+  methods: {
+    addProperty() {
+      this.form.properties.push(propertyTemplate())
+    },
+
+    deleteProperty(index) {
+      this.form.properties.splice(index, 1)
+    }
+  }
+}
+</script>
