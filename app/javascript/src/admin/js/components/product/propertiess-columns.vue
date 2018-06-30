23624180a5e28949003321fe8a5acaa8d6d459fe
FunnyQ <funnyq@gmail.com>
2018-06-30 18:48:19 +0800

    :fire: basic properties should not appear in edit form.

--- app/javascript/src/admin/js/components/product/propertiess-columns.vue
+++ app/javascript/src/admin/js/components/product/propertiess-columns.vue
@@ -3,72 +3,74 @@
 .vc-product-propertiess-columns
   h4.section-title {{pageTitleLocaleText('admin', 'products', 'property_fields')}}
 
+  //- 基本商品屬性（體積、重量）
   .properties-wrapper
-    //- 基本商品屬性（體積、重量）
-    h5.sub-title {{pageTitleLocaleText('admin', 'products', 'basic_properties')}}
-    .property-fields(v-for="basicProperty in basicProperties")
-      .columns
-        .column
-          b-field(:label="attributeLocaleText('product', 'property_name')"
-                  :type="errors.errorClassAt('properties')")
-            b-input(type="text"
-                    :value="attributeLocaleText('product', basicProperty.name)"
-                    disabled)
-        .column.is-6
-          b-field(:label="attributeLocaleText('product', 'property_value')"
-                  :type="errors.errorClassAt('properties')"
-                  :message="errors.get(basicProperty.name)")
-            b-input(type="number"
-                    placeholder="e.g. 80"
-                    v-model="form[basicProperty.name]"
-                    data-behavior="product-width-value"
-                    @input="errors.clear(basicProperty.name)")
-        .column
-          b-field(:label="attributeLocaleText('product', 'property_unit')"
-                  :type="errors.errorClassAt('properties')")
-            b-input(type="text"
-                    :value="basicProperty.unit"
-                    disabled)
+    .basic-properties(v-if="form.model.isNewRecord()")
+      h5.sub-title {{pageTitleLocaleText('admin', 'products', 'basic_properties')}}
+      .property-fields(v-for="basicProperty in basicProperties")
+        .columns
+          .column
+            b-field(:label="attributeLocaleText('product', 'property_name')"
+                    :type="errors.errorClassAt('properties')")
+              b-input(type="text"
+                      :value="attributeLocaleText('product', basicProperty.name)"
+                      disabled)
+          .column.is-6
+            b-field(:label="attributeLocaleText('product', 'property_value')"
+                    :type="errors.errorClassAt('properties')"
+                    :message="errors.get(basicProperty.name)")
+              b-input(type="number"
+                      placeholder="e.g. 80"
+                      v-model="form[basicProperty.name]"
+                      data-behavior="product-width-value"
+                      @input="errors.clear(basicProperty.name)")
+          .column
+            b-field(:label="attributeLocaleText('product', 'property_unit')"
+                    :type="errors.errorClassAt('properties')")
+              b-input(type="text"
+                      :value="basicProperty.unit"
+                      disabled)
     //- 自訂商品屬性
-    h5.sub-title(v-if="form.properties.length > 0") {{pageTitleLocaleText('admin', 'products', 'extra_properties')}}
-    .property-fields(v-for="(property, index) in form.properties")
-      .counter {{index + 1}}
-      .delete-button(@click="deleteProperty(index)")
-        i.fa.fa-close
-      .columns
-        .column
-          b-field(:label="attributeLocaleText('product', 'property_name')"
-                  :type="errors.errorClassAt('properties')"
-                  :message="errors.get('properties')"
-                  class="required")
-            b-input(type="text"
-                    placeholder="e.g. Battery"
-                    v-model="property.name"
-                    data-behavior="product-property-name"
-                    @input="errors.clear('properties')")
-        .column.is-6
-          b-field(:label="attributeLocaleText('product', 'property_value')"
-                  :type="errors.errorClassAt('properties')"
-                  :message="errors.get('properties')"
-                  class="required")
-            b-input(type="text"
-                    placeholder="e.g. 12000"
-                    v-model="property.value"
-                    data-behavior="product-property-value"
-                    @input="errors.clear('properties')")
-        .column
-          b-field(:label="attributeLocaleText('product', 'property_unit')"
-                  :type="errors.errorClassAt('properties')")
-            b-input(type="text"
-                    placeholder="e.g. mAh"
-                    v-model="property.unit"
-                    data-behavior="product-property-unit")
+    .extra-properties
+      h5.sub-title(v-if="form.properties.length > 0") {{pageTitleLocaleText('admin', 'products', 'extra_properties')}}
+      .property-fields(v-for="(property, index) in form.properties")
+        .counter {{index + 1}}
+        .delete-button(@click="deleteProperty(index)")
+          i.fa.fa-close
+        .columns
+          .column
+            b-field(:label="attributeLocaleText('product', 'property_name')"
+                    :type="errors.errorClassAt('properties')"
+                    :message="errors.get('properties')"
+                    class="required")
+              b-input(type="text"
+                      placeholder="e.g. Battery"
+                      v-model="property.name"
+                      data-behavior="product-property-name"
+                      @input="errors.clear('properties')")
+          .column.is-6
+            b-field(:label="attributeLocaleText('product', 'property_value')"
+                    :type="errors.errorClassAt('properties')"
+                    :message="errors.get('properties')"
+                    class="required")
+              b-input(type="text"
+                      placeholder="e.g. 12000"
+                      v-model="property.value"
+                      data-behavior="product-property-value"
+                      @input="errors.clear('properties')")
+          .column
+            b-field(:label="attributeLocaleText('product', 'property_unit')"
+                    :type="errors.errorClassAt('properties')")
+              b-input(type="text"
+                      placeholder="e.g. mAh"
+                      v-model="property.unit"
+                      data-behavior="product-property-unit")
 
-  .add-property-button.button.is-default.is-block(@click="addProperty"
-                                                  data-behavior="product-add-property-button")
-    .icon
-      i.fa.fa-plus
-    span {{actionLocaleText('admin', 'add_product_property')}}
+    .add-property-button.button.is-default.is-block(@click="addProperty"
+                                                    data-behavior="product-add-property-button")
+      .icon
+        i.fa.fa-plus
+      span {{actionLocaleText('admin', 'add_product_property')}}
 
 </template>
 
