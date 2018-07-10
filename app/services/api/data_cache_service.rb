e6695c9f898f17f5a049db10e173906759b6adaf
FunnyQ <funnyq@gmail.com>
2018-07-10 14:37:43 +0800

    :sparkles: add includes option for data cache service

--- app/services/api/data_cache_service.rb
+++ app/services/api/data_cache_service.rb
@@ -1,6 +1,6 @@
 # 嘗試先看看 request 的內容有沒有 cached 的資料，有的話使用 cache，沒有的話才 render json
 class Api::DataCacheService
-  attr_reader :collection, :request, :meta_options, :extra_options, :user_scope
+  attr_reader :collection, :request, :meta_options, :extra_options, :user_scope, :includes
 
   def initialize(collection, request, **options)
     @collection = collection
@@ -8,13 +8,14 @@ class Api::DataCacheService
 
     @meta_options = options.fetch(:meta, {})
     @extra_options = options.fetch(:extra, {})
+    @includes = options.fetch(:includes, [nil])
     @user_scope = options.fetch(:user_scope, nil)
   end
 
   def call
     Rails.cache.fetch(cache_key_with_user_scope) do
       show_indicator_in_development_log
-      ActiveModelSerializers::SerializableResource.new(collection, structured_options).as_json
+      ActiveModelSerializers::SerializableResource.new(collection.includes(*includes), structured_options).as_json
     end
   end
 
@@ -25,7 +26,7 @@ class Api::DataCacheService
   # @param [Object] collection ActiveRecord::Relation 物件或是單筆 model 的 instance。通常是使用 FetchingDataService 回傳的內容
   # @see FetchingDataService.call
   # @param [Object] request 從 controller 中得到的 request 物件
-  # @param [Hash] options 可傳入要給 AMS 的額外 options。 options[:meta] 會加入 AMS 的 meta 中，而 options[:extra] 則是 AMS 的 include 內容與 instance_options。options[:user_scope] 可指定目前使用 API 的使用者，一般可使用 Model class name。
+  # @param [Hash] options 可傳入要給 AMS 的額外 options。 options[:meta] 會加入 AMS 的 meta 中，而 options[:extra] 則是 AMS 的 include 內容與 instance_options。options[:user_scope] 可指定目前使用 API 的使用者，一般可使用 Model class name。options[:includes] 是 ActiveRecord 要 Query 時需要的 includes 內容，以 Array 的方式傳入即可，這樣僅會在沒有 Cache 時 eager load resources.
   #
   #
   # @example 要一併 render 出 Article 的 Comments，並在 meta 中加入 comment_count
@@ -35,7 +36,8 @@ class Api::DataCacheService
   #                                         request,
   #                                         meta: { article_count: Article.count },
   #                                         extra: { include: [:comments],
-  #                                                  show_comments: true })
+  #                                                  show_comments: true },
+  #                                         includes: [:comments]),
   #     render json: result
   #   # ...
   #
@@ -45,7 +47,8 @@ class Api::DataCacheService
   #     result = Api::DataCacheService.call(@articles,
   #                                         request,
   #                                         extra: extra_options,
-  #                                         user_scope: current_api_user.class.name)
+  #                                         user_scope: current_api_user.class.name,
+  #                                         includes: [:comments])
   #     render json: result
   #   # ...
   #   def extra_options
