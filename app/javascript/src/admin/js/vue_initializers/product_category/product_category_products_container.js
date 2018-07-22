d58713657286a160e1a51742dffb3baf9a15477b
FunnyQ <funnyq@gmail.com>
2018-07-22 13:42:43 +0800

    :sparkles: add new sub category button for product category detail page

--- app/javascript/src/admin/js/vue_initializers/product_category/product_category_products_container.js
+++ app/javascript/src/admin/js/vue_initializers/product_category/product_category_products_container.js
@@ -1,11 +1,13 @@
 import ProductCategory from '../../../../shared/resource_models/product_category.js'
 import ProductsListTable from '../../components/product_category/products-list-table.vue'
+import NewSubCategoryButton from '../../components/product_category/new-sub-category-button.vue'
 
 export default {
   name: 'product-category-products-container',
 
   components: {
-    ProductsListTable
+    ProductsListTable,
+    NewSubCategoryButton
   },
 
   // mixins: [],
